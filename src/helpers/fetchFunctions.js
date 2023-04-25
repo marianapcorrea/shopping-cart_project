c02991f9217480c1e2d081a5c88716ae183093fe
Mariana Correa <mpcs270391@gmail.com>
2023-04-25 16:56:11 -0300

    Requirements 1 and 2

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,6 +2,17 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+export const fetchProductsList = async (item) => {
+  // try {
+  if (!item) {
+    throw new Error('Termo de busca não informado');
+  }
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${item}`);
+  const data = await response.json();
+  return data.results;
+/*   } catch (error) {
+    console.log(error.message);
+    throw error.message;
+    // return error.message;
+  } */
 };
