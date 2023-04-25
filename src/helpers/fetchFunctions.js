cdcba51bd71e9febe8ade88fe2c12a07e9110087
Mariana Correa <mpcs270391@gmail.com>
2023-04-25 16:57:12 -0300

    Clean commented codes in fetchFunctions.js

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -3,16 +3,10 @@ export const fetchProduct = () => {
 };
 
 export const fetchProductsList = async (item) => {
-  // try {
   if (!item) {
     throw new Error('Termo de busca não informado');
   }
   const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${item}`);
   const data = await response.json();
   return data.results;
-/*   } catch (error) {
-    console.log(error.message);
-    throw error.message;
-    // return error.message;
-  } */
 };
