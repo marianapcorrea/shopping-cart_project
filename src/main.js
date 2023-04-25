a4baad53051dc98e2e51ff586bb768d31eeaf7c4
Mariana Correa <mpcs270391@gmail.com>
2023-04-25 17:38:37 -0300

    Requirement 3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { searchCep } from './helpers/cepFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const populateProductSection = async (product) => {
+  const result = await fetchProductsList(product);
+  console.log(result);
+  const products = document.querySelector('.products');
+  // return createProductElement(result);
+  result.map((item) => products.appendChild(createProductElement(item)));
+};
+
+populateProductSection('computador');
