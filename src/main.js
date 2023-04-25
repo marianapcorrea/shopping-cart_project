5d0836149653f648432ba991b08986c89b068692
Mariana Correa <mpcs270391@gmail.com>
2023-04-25 20:53:27 -0300

    Requirement 4

--- src/main.js
+++ src/main.js
@@ -3,14 +3,25 @@ import { fetchProductsList } from './helpers/fetchFunctions';
 import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
-document.querySelector('.cep-button').addEventListener('click', searchCep);
+const products = document.querySelector('.products');
+
+const addLoadingText = () => {
+  const loading = document.createElement('p');
+  loading.innerText = 'carregando...';
+  loading.className = 'loading';
+  products.appendChild(loading);
+};
+
+const removeLoadingText = () => document.querySelector('.loading').remove();
 
 const populateProductSection = async (product) => {
   const result = await fetchProductsList(product);
-  console.log(result);
-  const products = document.querySelector('.products');
-  // return createProductElement(result);
   result.map((item) => products.appendChild(createProductElement(item)));
+  removeLoadingText();
 };
 
-populateProductSection('computador');
+window.onload = () => {
+  addLoadingText();
+  populateProductSection('computador');
+  document.querySelector('.cep-button').addEventListener('click', searchCep);
+};
