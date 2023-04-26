466f3dd6d1d5faf2245bc98702d226554895578d
Mariana Correa <mpcs270391@gmail.com>
2023-04-25 21:36:51 -0300

    Requirement 6

--- src/tests/fetchProduct.test.js
+++ src/tests/fetchProduct.test.js
@@ -4,7 +4,23 @@ import product from './mocks/product';
 
 // implemente seus testes aqui
 describe('Teste a função fetchProduct', () => {
-  it('...', () => {
-    
+  it('fetchProduct é uma função', () => {
+    expect(typeof (fetchProduct)).toBe('function');
+  });
+  it('fetch é chamado ao executar fetchProduct', () => {
+    fetchProduct('MLB1405519561');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('fetch é chamado com o endpoint correto ao executar fetchProduct', () => {
+    const endpoint = 'https://api.mercadolibre.com/items/MLB1405519561';
+    fetchProduct('MLB1405519561');
+    expect(fetch).toHaveBeenCalledWith(endpoint);
+  });
+  it('O retorno de "fetchProduct(computador)" === product', () => {
+    expect(fetchProduct('MLB1405519561')).resolves.toBe(product);
+  });
+  it('Chamar fetchProduct() retorna erro:Termo de busca não informado', async () => {
+    // const promise = ;
+    await expect(fetchProduct()).rejects.toThrow('ID não informado');
   });
 });
